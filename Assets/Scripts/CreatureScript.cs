f802ae7a87c704cc9017ab6c2ca151cbf1525e47
Henrybcool <henryfehlner5@gmail.com>
2025-05-24 11:15:23 +0900

    added scritps folder and started creature script

---
+++ Assets/Scripts/CreatureScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatureScript : MonoBehaviour
+{
+    [SerializeField] private Vector2 startPosition;
+    [SerializeField] private Vector2 velocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
