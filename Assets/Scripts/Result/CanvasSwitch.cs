b6c6d9f235def2b3b989bef923bf156f10ec81f7
karasu5555 <kakerukk98@gmail.com>
2025-05-24 17:10:30 +0900

    Tentative implementation of Result2

---
+++ Assets/Scripts/Result/CanvasSwitch.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasSwitch : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] canvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown&& canvas[0].activeSelf==true)
+        {
+            canvas[0].SetActive(false);
+            canvas[1].SetActive(true);
+        }
+    }
+}
