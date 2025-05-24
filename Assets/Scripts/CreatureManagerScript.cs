54b748725ccdcee4f83bf8e0d3f612be2942d46f
DDDpega <133212827+DDDpega@users.noreply.github.com>
2025-05-24 13:57:07 +0900

    Spewn Creature Interval

--- Assets/Scripts/CreatureManagerScript.cs
+++ Assets/Scripts/CreatureManagerScript.cs
@@ -4,19 +4,58 @@ using UnityEngine;
 
 public class CreatureManagerScript : MonoBehaviour
 {
-    [SerializeField] private GameObject creaturePrefab;
+	[SerializeField] private GameObject creaturePrefab;
 
-    private List<GameObject> creatureList = new List<GameObject>();
+	[SerializeField] private GameObject gameManager;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        creatureList.Add(Instantiate(creaturePrefab));
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	[SerializeField, Tooltip("second")] private int[] spawnIntervalTimeArray;
+	private int speedUpLevel;
+	private int frame;
+	[SerializeField, Tooltip("second")] private int speedUpIntervalTime;
+
+	private List<GameObject> creatureList = new List<GameObject>();
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		//setting 60 fps
+		QualitySettings.vSyncCount = 0;
+		Application.targetFrameRate = 60;
+
+
+		CreateCreature();
+		speedUpLevel = frame = 0;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		//pause
+		if (gameManager.GetComponent<GameManager>().isPaused) return;
+
+		++frame;
+
+		//spawn Creature
+		if (frame % spawnIntervalTimeArray[speedUpLevel] == 0) 
+        {
+			CreateCreature();
+        }
+
+		if (frame % speedUpIntervalTime == 0)
+        {
+			if (spawnIntervalTimeArray.Length != speedUpLevel + 1)
+            {
+				++speedUpLevel;
+				Debug.Log("Speed Up");
+			}
+				
+		}
+	}
+
+	//Spawn Creature and Add Creature List
+	void CreateCreature()
+	{
+		creatureList.Add(Instantiate(creaturePrefab));
+	}
 }
