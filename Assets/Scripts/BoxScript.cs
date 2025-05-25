b0a4a1cb808455ddf6b0a083d68b02bc63b75974
DDDpega <133212827+DDDpega@users.noreply.github.com>
2025-05-25 13:28:10 +0900

    bug fixed

--- Assets/Scripts/BoxScript.cs
+++ Assets/Scripts/BoxScript.cs
@@ -162,16 +162,17 @@ public class BoxScript : MonoBehaviour
 		{
 			ScoreScript.instance.addToScore(hitCreature.GetComponent<CreatureScript>().IsRainbow);
 		}
-		else if (hitCreature.GetComponent<CreatureScript>().Type == "fuwa" && boxNumber == 1)
-		{
-			TigerAnimationHandler.instance.rightPain();
-		}
-        else if (hitCreature.GetComponent<CreatureScript>().Type == "moco" && boxNumber == 2)
-        {
-            TigerAnimationHandler.instance.leftPain();
-        }
+
 		else
 		{
+			if (hitCreature.GetComponent<CreatureScript>().Type == "fuwa" && boxNumber == 1)
+			{
+				TigerAnimationHandler.instance.rightPain();
+			}
+			else if (hitCreature.GetComponent<CreatureScript>().Type == "moco" && boxNumber == 2)
+			{
+				TigerAnimationHandler.instance.leftPain();
+			}
 			//different kind
 			ScoreScript.instance.breakCombo();
 
